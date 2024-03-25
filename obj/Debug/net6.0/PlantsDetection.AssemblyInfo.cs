6132284b0ba31b15e7b063884acb37b02ccc8ab1
DoaaSaeed55 <143820048+DoaaSaeed55@users.noreply.github.com>
2024-03-25 14:47:40 +0200

    ..

--- obj/Debug/net6.0/PlantsDetection.AssemblyInfo.cs
+++ obj/Debug/net6.0/PlantsDetection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlantsDetection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f765d850f62fcaf5bb35a34a3edb0864fa4c02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlantsDetection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlantsDetection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
